205f553b71660782aaf94a7795550ea11817a991
Durgesh Pandey <durgeshpandey@Durgeshs-MacBook-Pro.local>
2024-03-28 21:43:42 +0530

    fetch and update route added

--- controllers/userController.js
+++ controllers/userController.js
@@ -23,11 +23,31 @@ const getContacts = asyncHandler(async(req,res)=>{
 });
 
 const getContact = asyncHandler(async(req,res)=>{
-    console.log("Get contact details by ID")
+    
+    try {
+        const {id} = req.params;
+        const contact = await Contact.findById(id);
+        res.status(200).json(contact);
+        console.log(contact)
+    } catch (error) {
+        console.error("Error creating contact:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+    
 });
 
 const updateContact = asyncHandler(async(req,resp)=>{
-    console.log("Update contact details")
+    try {
+        const {id} = req.params;
+        const contact = await Contact.findByIdAndUpdate(id,req.body)
+        if (!contact){
+            res.status(404).json({error: "Contact not Found"});
+            return;
+        }
+    } catch (error) {
+        console.error("Error updating the contact", error);
+        res.status(500).json({error: "Internal Server Error"});
+    }
 });
 
 const deleteContact = asyncHandler(async(req,res)=>{
